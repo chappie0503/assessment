3c90672461afb8bbfdeabe5edb8be65072d30570
chappie0503 <45314812+chappie0503@users.noreply.github.com>
2020-06-08 10:25:34 +0900

    #1に対する対応をしました

--- assessment.js
+++ assessment.js
@@ -87,6 +87,7 @@ function assessment(userName) {
   // 文字のコード番号の合計を回答の数で割って添字の数値を求める
   const index = sumOfcharCode % answers.length;
   let result = answers[index];
+  
 
   result = result.replace(/{userName}/g, userName);
   return result;
